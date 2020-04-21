0f1c361925e09e20ea1f55c6775dce75186e01c4
gabbartose <gabrijel.bartosek@gmail.com>
2020-04-21 09:23:58 +0200

    Bug fix

--- Controller/RepositoryMasterViewController.swift
+++ Controller/RepositoryMasterViewController.swift
@@ -14,7 +14,6 @@ class RepositoryMasterViewController: UITableViewController {
     }
 
     let identifier: "RepositoryCell"
-    let created
 
     override func viewDidLoad() {
         super.viewDidLoad()
